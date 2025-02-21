c47c9f51d170bd0755615d1b0afa872969e4d024
juancarlosalvarezbueno <juancarlos.alvarez.bueno@gmail.com>
2025-02-21 22:12:15 +0100

    ultima modificación

--- spotify_app/views.py
+++ spotify_app/views.py
@@ -1,8 +1,6 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from django.http import JsonResponse
-from django.shortcuts import redirect
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 import json
 import random
